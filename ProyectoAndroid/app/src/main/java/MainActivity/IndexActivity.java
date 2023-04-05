0faeb4df123b3e6cf645624a359cc9505258e074
Zharell <javi_lopez-calderon@hotmail.com>
2023-04-05 19:19:42 +0200

    IndexActivity añadido, revisar recursos, utilización de drawable

--- ProyectoAndroid/app/src/main/java/MainActivity/IndexActivity.java
+++ ProyectoAndroid/app/src/main/java/MainActivity/IndexActivity.java
@@ -1,14 +1,22 @@
-package com.example.marfol;
+package MainActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.marfol.R;
+
+
+public class IndexActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_index);
+
+
     }
+
+
+
 }
