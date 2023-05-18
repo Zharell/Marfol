6d1d035e65742cbdb047c3192012098a325d8216
Relyak <kaylerdavid@gmail.com>
2023-05-18 23:24:56 +0200

    amgaga

--- ProyectoAndroid/app/src/main/java/mainActivity/MetodosGlobales.java
+++ ProyectoAndroid/app/src/main/java/mainActivity/MetodosGlobales.java
@@ -1,21 +1,14 @@
 package mainActivity;
 import android.content.Context;
-import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
-import androidx.annotation.NonNull;
 import com.bumptech.glide.Glide;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.tfg.marfol.R;
-
-import org.w3c.dom.Text;
 
 public class MetodosGlobales {
     public static void cambiarImagenSiLogueado(Context context, ImageView iVimagen) {
@@ -24,34 +17,31 @@ public class MetodosGlobales {
         if (mAuth.getCurrentUser() != null) {
             FirebaseUser currentUser = mAuth.getCurrentUser();
             DocumentReference userRef = db.collection("users").document(currentUser.getEmail());
-            userRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                @Override
-                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                    if (task.isSuccessful()) {
-                        DocumentSnapshot document = task.getResult();
-                        if (document.exists()) {
-                            String imagen = document.getString("imagen");
-                            if (imagen != null&& !imagen.equalsIgnoreCase("")) {
-                                iVimagen.setBackground(null);
-                                iVimagen.setClickable(false);  // Deshabilitar el clic en la imagen
-                                iVimagen.setFocusable(false);
-                                iVimagen.setPadding(20, 20, 20, 20);
-                                Glide.with(context)
-                                        .load(imagen)
-                                        .circleCrop() // Aplica el formato redondeado
-                                        .into(iVimagen);
-                            } else {
-                                Glide.with(context)
-                                        .load(R.drawable.nologinimg)
-                                        .circleCrop() // Aplica el formato redondeado
-                                        .into(iVimagen);
-                            }
+            userRef.get().addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        String imagen = document.getString("imagen");
+                        if (imagen != null&& !imagen.equalsIgnoreCase("")) {
+                            iVimagen.setBackground(null);
+                            iVimagen.setClickable(false);  // Deshabilitar el clic en la imagen
+                            iVimagen.setFocusable(false);
+                            iVimagen.setPadding(20, 20, 20, 20);
+                            Glide.with(context)
+                                    .load(imagen)
+                                    .circleCrop() // Aplica el formato redondeado
+                                    .into(iVimagen);
                         } else {
-                            Glide.with(context).load(R.drawable.nologinimg).into(iVimagen);
+                            Glide.with(context)
+                                    .load(R.drawable.nologinimg)
+                                    .circleCrop() // Aplica el formato redondeado
+                                    .into(iVimagen);
                         }
                     } else {
-                        Toast.makeText(context, "Error al obtener los datos del usuario", Toast.LENGTH_SHORT).show();
+                        Glide.with(context).load(R.drawable.nologinimg).into(iVimagen);
                     }
+                } else {
+                    Toast.makeText(context, "Error al obtener los datos del usuario", Toast.LENGTH_SHORT).show();
                 }
             });
         } else {
